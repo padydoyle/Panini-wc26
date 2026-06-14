65bfbc66ef876522c6436c5a345e416248c19553
padydoyle <doylepatrick88@gmail.com>
2026-06-14 08:19:34 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'panini-wc26-v17';
+const CACHE_NAME = 'panini-wc26-v19';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
